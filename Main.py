1c86447e3788df0655fd4ceda9b64d6219c1eef3
Robert Tian <robertian23@berkeley.edu>
2024-10-28 17:44:22 -0700

    more docker setup

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import molicell  # Assuming molicell.py contains the main logic
 import energus 
 import open_loop
+import accumulator
 
 def main():
     molicell.plot()  # Call the function that generates the plot
     energus.plot()
-    open_loop()
+    open_loop.simulate(accumulator.Pack())
     
 
 if __name__ == "__main__":
