99c1d6996d08fb38a972479d535f127ad62467d4
Joebangles <qhcbest@gmail.com>
2025-03-20 11:40:35 -0700

    Slip Angle data analysis

--- tire-sim/QTireSim.py
+++ tire-sim/QTireSim.py
@@ -1,4 +1,5 @@
 from typing import List
+import sys
 
 import pandas as pd
 import numpy as np
@@ -11,95 +12,203 @@ from plotly.subplots import make_subplots
 def pacejka_model(alpha, B, C, D, E, F):
     return D * np.sin(C * np.arctan(B * alpha - E * (B * alpha - np.arctan(B * alpha))) + F)
 
+def B_fit(Fz, B1, B2):
+    return B1 + Fz * B2
+
+def C_fit(Fz, C1, C2):
+    return C1 + Fz * C2
+
+def D_fit(Fz, D1, D2):
+    return (D1 * Fz) + D2 * (Fz ** 2)
+
+def E_fit(Fz, E1, E2):
+    return E1 + Fz * E2
+
 np.set_printoptions(suppress=True) # Disable scientific notation
 
-FZ_BIN_COUNT = 4
-BIN_COLORS = ["red", "green", "blue", "purple", "pink", "orange"]
+FZ_BIN_COUNT = 5
+BIN_COLORS = ["red", "green", "blue", "purple", "pink", "orange", "black", "gray"]
+GRAPH_COLS = 3
+GRAPH_ROWS = 2
 
-PACEJKA_PARAMS_GUESS = [0.1, 0.1, 100, 0.1, 0]  # Update as needed
-PACEJKA_PARAMS_BOUNDS = ([0, 0, 0, 0, 0],  [np.inf, np.inf, np.inf, np.inf, np.inf])
+PACEJKA_PARAMS_NAMES = ["B", "C", "D", "E", "F"]
+PACEJKA_PARAMS_GUESS = [-0.1, 0.1, 2000, 0.3, 0]  # Update as needed
+PACEJKA_PARAM_FIT_FNS = [B_fit, C_fit, D_fit, E_fit]
 
-RUN_NUM = 51
+# RUN_NUM = 9 # First nine runs are all using our desired 16 x 7.5 tires
+SELECTED_RUNS = [4, 5, 6, 8, 9]
 
-# Import the data
-metric_data = pd.read_csv(f'./RunData_DriveBrake_ASCII_SI_Round9/B2356run{RUN_NUM}.dat', skiprows=1, sep='\t')
+def get_run_data(selected_runs : List[int]) -> pd.DataFrame:
+    metric_data = pd.DataFrame()
 
-# Rename columns to have value and units, and remove the previous row for units
-metric_data.rename(columns={col: f"{col} {metric_data[col][0]}" for col in metric_data.columns}, inplace=True)
-metric_data = metric_data.drop(0)
+    for rn in selected_runs:
+        # Import the data
+        metric_datum = pd.read_csv(f'./RunData_Cornering_ASCII_SI_Round9/B2356run{rn}.dat', skiprows=1, sep='\t')
 
-# Convert all values to floats
-metric_data = metric_data.apply(pd.to_numeric, errors='coerce')
+        # Rename columns to have value and units, and remove the previous row for units
+        metric_datum.rename(columns={col: f"{col} {metric_datum[col][0]}" for col in metric_datum.columns}, inplace=True)
+        metric_datum = metric_datum.drop(0)
 
-# Drop any rows with NaN values (if necessary)
-metric_data = metric_data.dropna()
+        # Convert all values to floats
+        metric_datum = metric_datum.apply(pd.to_numeric, errors='coerce')
 
-# Reset index
-metric_data.reset_index(drop=True, inplace=True)
+        # Drop any rows with NaN values (if necessary)
+        metric_datum = metric_datum.dropna()
 
-print(metric_data.columns)
-print(metric_data)
+        # Reset index
+        metric_datum.reset_index(drop=True, inplace=True)
 
-# IQR filter Slip Angle and Fy
-Q1 = metric_data[['SA deg', 'FY N']].quantile(0.25)
-Q3 = metric_data[['SA deg', 'FY N']].quantile(0.75)
-IQR = Q3 - Q1
+        # print(metric_datum.columns)
+        # print(metric_datum)
 
-metric_data = metric_data[~((metric_data[['SA deg', 'FY N']] < (Q1 - 1.5 * IQR)) | (metric_data[['SA deg', 'FY N']] > (Q3 + 1.5 * IQR))).any(axis=1)]
+        # IQR filter Slip Angle and Fy
+        Q1 = metric_datum[['SA deg', 'FY N']].quantile(0.25)
+        Q3 = metric_datum[['SA deg', 'FY N']].quantile(0.75)
+        IQR = Q3 - Q1
 
-# Define bins by evenly splitting range
+        # metric_datum = metric_datum[~((metric_datum[['SA deg', 'FY N']] < (Q1 - 1.5 * IQR)) | (metric_datum[['SA deg', 'FY N']] > (Q3 + 1.5 * IQR))).any(axis=1)]
 
-# Fz_min = metric_data["FZ N"].min()
-# Fz_max = metric_data["FZ N"].max()
-# Fz_range = Fz_max - Fz_min
+        metric_data = pd.concat([metric_data, metric_datum])
+        print(len(metric_datum))
 
-# Fz_bin_points = np.linspace(Fz_min, Fz_max, FZ_BIN_COUNT)
+    return metric_data
 
-# Define bins by splitting by percentages of data in each bin
+def get_bins(metric_data : pd.DataFrame) -> List[tuple[int, int]]:
+    # Define bins by evenly splitting range
 
-Fz_bin_points = [metric_data["FZ N"].quantile(p) for p in np.linspace(0, 1, FZ_BIN_COUNT + 1)]
+    # Fz_min = metric_data["FZ N"].min()
+    # Fz_max = metric_data["FZ N"].max()
+    # Fz_range = Fz_max - Fz_min
 
-Fz_bins = [[lb, ub] for lb, ub in zip(Fz_bin_points[:-1], Fz_bin_points[1:])]
+    # Fz_bin_points = np.linspace(Fz_min, Fz_max, FZ_BIN_COUNT)
 
-all_data_binned = []
-params_binned = []
+    # Define bins by splitting by percentages of data in each bin
 
-# Sort data into bins, and find coefficients
-for Fz_bin in Fz_bins:
-    lb = Fz_bin[0]
-    ub = Fz_bin[1]
+    Fz_bin_points = [metric_data["FZ N"].quantile(p) for p in np.linspace(0, 1, FZ_BIN_COUNT + 1)]
 
-    data_binned = metric_data[(lb <= metric_data["FZ N"]) & (metric_data["FZ N"] < ub)][["SA deg", "FY N"]].values.T
-    all_data_binned.append(data_binned)
+    Fz_bins = [[lb, ub] for lb, ub in zip(Fz_bin_points[:-1], Fz_bin_points[1:])]
 
-    params_optimal, _ = curve_fit(pacejka_model, data_binned[0], data_binned[1], p0=PACEJKA_PARAMS_GUESS, maxfev=1500000)
-    params_binned.append(params_optimal)
+    return Fz_bins
 
-# Draw the data
-# fig = go.Figure()
-fig = make_subplots(rows=2, cols=2, subplot_titles=[f"Fy over SA for Fz in bin [{lb:.2f}, {ub:.2f})" for lb, ub in Fz_bins])
+def Fz_bin_data(metric_data : pd.DataFrame, Fz_bins : List[List[int]]) -> List[pd.DataFrame]:
+    metric_data_binned = []
+    # Sort data into bins, and find coefficients
+    for Fz_bin in Fz_bins:
+        lb = Fz_bin[0]
+        ub = Fz_bin[1]
 
-for i in range(FZ_BIN_COUNT):
-    data_binned = all_data_binned[i]
-    graph_domain = np.linspace(min(data_binned[0]), max(data_binned[0]), 500)
+        data_binned = metric_data[(lb <= metric_data["FZ N"]) & (metric_data["FZ N"] < ub)][["SA deg", "FY N", "FZ N"]]
 
-    fig.add_trace(go.Scatter(x=data_binned[0], y=data_binned[1], mode="markers", marker=dict(color=BIN_COLORS[i]), name=f'Data for bin {i}'), row=i // 2 + 1, col=i % 2 + 1)
-    fig.add_trace(go.Scatter(x=graph_domain, y=pacejka_model(graph_domain, *params_binned[i]), mode="lines", marker=dict(color="yellow"), name=f'Fitted curve for bin {i}'), row=i // 2 + 1, col=i % 2 + 1)
+        metric_data_binned.append(data_binned)
+    
+    return metric_data_binned
 
-min_r2 = np.inf
+def get_pacejka_params(metric_data : pd.DataFrame) -> tuple[float, float, float, float]:
+    params_optimal, _ = curve_fit(pacejka_model, metric_data["SA deg"], metric_data["FY N"], p0=PACEJKA_PARAMS_GUESS, maxfev=100000)
+    return params_optimal
+
+def single_fig_color_gradient(metric_data : pd.DataFrame) -> go.Figure:
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=metric_data["SA deg"],
+            y=metric_data["FY N"],
+            mode="markers", 
+            marker=dict(color=metric_data["FZ N"], colorscale="Viridis", showscale=True, colorbar=dict(title="Load Force (N)")),
+        )
+    )
+
+    return fig
+
+def bin_figs(data_binned : List[pd.DataFrame], params_binned : List[tuple[float, float, float, float ,float]], Fz_bins : List[tuple[float, float]]) -> go.Figure:
+    fig = make_subplots(rows=GRAPH_ROWS, cols=GRAPH_COLS, subplot_titles=[f"Fy over SA for Fz in bin [{lb:.2f}, {ub:.2f})" for lb, ub in Fz_bins])
+
+    for i in range(FZ_BIN_COUNT):
+        data_bin = data_binned[i]
+        graph_domain = np.linspace(min(data_bin["SA deg"]), max(data_bin["SA deg"]), 500)
+
+        # Draw scatterplot of data
+        fig.add_trace(
+            go.Scatter( x=data_bin["SA deg"], 
+                        y=data_bin["FY N"], 
+                        mode="markers", 
+                        # marker=dict(
+                        #     color=data_bin["FZ N"], 
+                        #     colorscale="Viridis", 
+                        #     showscale=True, 
+                        #     colorbar=dict(title=f"Bin {i + 1}")
+                        # ),
+                        name=f'Data for bin {i}'
+            ), 
+            row=i // GRAPH_COLS + 1, 
+            col=i % GRAPH_COLS + 1
+        )
+
+        # Draw pacejka curve
+        fig.add_trace(
+            go.Scatter(
+                x=graph_domain, 
+                y=pacejka_model(graph_domain, *params_binned[i]), 
+                mode="lines", 
+                marker=dict(color="yellow"), 
+                name=f'Fitted curve for bin {i}'
+            ), 
+            row=i // GRAPH_COLS + 1, 
+            col=i % GRAPH_COLS + 1
+        )
+    
+    return fig
+
+def coefficient_figs(params_binned : List[tuple[float, float, float, float, float]], Fz_bins : List[tuple[float, float]]) -> go.Figure:
+    fig = make_subplots(rows=2, cols=3, subplot_titles=[f"{PACEJKA_PARAMS_NAMES[i]} value over load force bins" for i in range(5)])
+
+    for i in range(5):
+        Fz_medians = [(ub + lb) / 2 for lb, ub in Fz_bins]
+
+        fig.add_trace(go.Scatter(x=Fz_medians, y=[params_binned[j][i] for j in range(FZ_BIN_COUNT)]), row = i // 3 + 1, col = i % 3 + 1)
+        fig.update_xaxes(title_text=f"Fz Bin Median", row = i // 3 + 1, col = i % 3 + 1)
+        fig.update_yaxes(title_text=f"Value of param {PACEJKA_PARAMS_NAMES[i]}", row = i // 3 + 1, col = i % 3 + 1)
+
+        # Fit param data for the Pacejka model coefficients, we don't have a fit fn for F not sure why
+        if i < 4:
+            coefficients, _ = curve_fit(PACEJKA_PARAM_FIT_FNS[i], Fz_medians, [params_binned[j][i] for j in range(FZ_BIN_COUNT)])
+            Fz_domain = np.linspace(Fz_medians[0], Fz_medians[-1], 50)
+            fig.add_trace(go.Scatter(x=Fz_domain, y=PACEJKA_PARAM_FIT_FNS[i](Fz_domain, *coefficients)), row = i // 3 + 1, col = i % 3 + 1)
+
+    fig.update_layout(title_text=f"Binned Fy over SA for Fz bins on runs {SELECTED_RUNS}")
+
+    return fig
+
+all_data = get_run_data(SELECTED_RUNS)
+all_data = all_data[all_data["FZ N"] < -150]
+
+Fz_bins = [(-1300, -1000), (-1000, -750), (-750, -500), (-500, -300), (-300, -150)] # Empirically found by looking at graph.
+all_data_binned = Fz_bin_data(all_data, Fz_bins)
+
+params_binned = [get_pacejka_params(data_bin) for data_bin in all_data_binned]
+
+# new_fig = single_fig_color_gradient(all_data[all_data["FZ N"] < -1000])
+# new_fig.update_layout(
+#     title="Lateral Force over Slip Angle",
+#     xaxis_title="Slip Angle (deg)",
+#     yaxis_title="Lateral Force (N)"
+# )
+
+# new_fig.write_html("QVis.html")
+
+# new_fig = bin_figs(all_data_binned, params_binned, Fz_bins)
+# new_fig.write_html("QVis.html")
 
 for i in range(FZ_BIN_COUNT):
     print(f"Bin {i}")
-    
     print("Params:", params_binned[i])
 
-    r2_val = r2_score(all_data_binned[i][1], pacejka_model(all_data_binned[i][0], *params_binned[i]))
+    r2_val = r2_score(all_data_binned[i]["FY N"], pacejka_model(all_data_binned[i]["SA deg"], *params_binned[i]))
     print("R2:", r2_val)
 
-    if r2_val < min_r2:
-        min_r2 = r2_val
 
-print(min_r2)
+new_fig = coefficient_figs(params_binned, Fz_bins)
+new_fig.write_html("QVis.html")
 
-fig.update_layout(title_text=f"Binned Fy over SA for Fz bins on run {RUN_NUM}")
-fig.show()
+sys.exit(0)
