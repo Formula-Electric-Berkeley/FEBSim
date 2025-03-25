4151dc57e0956e8a8f3084e68dbc39f81e1a218f
Joebangles <qhcbest@gmail.com>
2025-03-24 19:22:56 -0700

    Update tire data tools

--- tire-sim/QTireSim_fx.py
+++ tire-sim/QTireSim_fx.py
@@ -8,26 +8,36 @@ from sklearn.metrics import r2_score
 import plotly.graph_objects as go # Nick did this, idk if it's standard but it's cute cause then you get go.Figure()
 # don't think it's standard but it's funny
 from plotly.subplots import make_subplots
-import QTireSim as fy
 
-# Define the Pacejka Magic Formula with five coefficients
-# slip angle for lateral (Fy), slip ratio for longitudinal (Fx)
-def pacejka_model(sr, B, C, D, E, F):
-    return D * np.sin(C * np.arctan(B * sr - E * (B * sr - np.arctan(B * sr))) + F)
+def format_float(x, sig_digits=3):
+    # Round to 3 significant digits
+    rounded = f"{x:5G}"
+    return str(float(rounded))
 
-def B_fit(Fz, B1, B2):
-    return B1 + Fz * B2
+# Define the Pacejka Magic Formula for lateral force (FY) with five coefficients
+def pacejka_model(kappa, B, C, D, E, F):
+    return D * np.sin(C * np.arctan(B * kappa - E * (B * kappa - np.arctan(B * kappa))) + F)
 
-def C_fit(Fz, C1, C2):
-    return C1 + Fz * C2
+def quad_fit(Fz, a0, a1, a2):
+    return a0 + a1 * Fz + a2 * (Fz ** 2)
 
-def D_fit(Fz, D1, D2):
-    return (D1 * Fz) + D2 * (Fz ** 2)
+def lin_fit(Fz, a0, a1):
+    return a0 + Fz * a1
 
-def E_fit(Fz, E1, E2):
-    return E1 + Fz * E2
+def exp_fit(Fz, a0, a1, a2):
+    return a0 + a1 * (np.e ** (a2 * Fz))
 
-np.set_printoptions(suppress=True) # Disable scientific notation
+def fit_print(ftype, *params):
+    params = np.vectorize(format_float)(params)
+    if ftype == quad_fit:
+        return f"{params[0]} + {params[1]} F_z + {params[2]} F_z^2"
+    elif ftype == lin_fit:
+        return f"{params[0]} + {params[1]} F_z"
+    else:
+        return f"{params[0]} + {params[1]} exp({params[2]} F_z)"
+
+np.set_printoptions(suppress=True, precision=5) # Disable scientific notation
+pd.options.plotting.backend = "plotly"
 
 FZ_BIN_COUNT = 5
 BIN_COLORS = ["red", "green", "blue", "purple", "pink", "orange", "black", "gray"]
@@ -35,18 +45,27 @@ GRAPH_COLS = 3
 GRAPH_ROWS = 2
 
 PACEJKA_PARAMS_NAMES = ["B", "C", "D", "E", "F"]
-PACEJKA_PARAMS_GUESS = [-0.1, 0.1, 2000, 0.3, 0]  # Update as needed
-PACEJKA_PARAM_FIT_FNS = [B_fit, C_fit, D_fit, E_fit]
+PACEJKA_PARAMS_GUESS = [-18, -0.01, 100000, -2, 0.00076]  # Update as needed
 
- # runs w/ 18.0X6.0 tire data
-SELECTED_RUNS = [68, 71, 72, 73]
+PACEJKA_PARAM_FIT_FNS = [exp_fit, quad_fit, quad_fit, lin_fit, lin_fit]
+PACEJKA_PARAM_FIT_GUESS = [
+    [-18, -30, 0.01],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0],
+    [0, 0]
+]
+
+# runs w/ 18.0X6.0 tire data
+SELECTED_RUNS = [72, 73]
+k_y = 1.059952828992417 # see notion for derivation
 
 def get_run_data(selected_runs : List[int]) -> pd.DataFrame:
     metric_data = pd.DataFrame()
 
     for rn in selected_runs:
         # Import the data
-        metric_datum = pd.read_csv(f'./RawData_DriveBrake_ASCII_SI_Round9/B2356raw{rn}.dat', skiprows=1, sep='\t')
+        metric_datum = pd.read_csv(f'./RunData_DriveBrake_ASCII_SI_Round9/B2356run{rn}.dat', skiprows=1, sep='\t')
 
         # Rename columns to have value and units, and remove the previous row for units
         metric_datum.rename(columns={col: f"{col} {metric_datum[col][0]}" for col in metric_datum.columns}, inplace=True)
@@ -61,22 +80,17 @@ def get_run_data(selected_runs : List[int]) -> pd.DataFrame:
         # Reset index
         metric_datum.reset_index(drop=True, inplace=True)
 
-        # print(metric_datum.columns)
-        # print(metric_datum)
-
-        # IQR filter Slip ratio and Fx
-        # print(metric_datum.columns)
-
-        Q1 = metric_datum[['SR SAE', 'FX N']].quantile(0.25)
-        Q3 = metric_datum[['SR SAE', 'FX N']].quantile(0.75)
-        IQR = Q3 - Q1
-        
-
-        # filter outliers
-        # metric_datum = metric_datum[~((metric_datum[['SR SAE', 'FX N']] < (Q1 - 1.5 * IQR)) | (metric_datum[['SR SAE', 'FX N']] > (Q3 + 1.5 * IQR))).any(axis=1)]
+        # Fill in SL data if necessary
+        # if "SL none" not in metric_datum.columns:
+        #     metric_datum["SL none"] =  (metric_datum["RE cm"] / metric_datum["RL cm"]) * (metric_datum["SR SAE"] + 1) - 1
 
         metric_data = pd.concat([metric_data, metric_datum])
         print(len(metric_datum))
+    
+    
+    # Rescale forces for tire diam
+    metric_data["FX (unscaled) N"] = metric_data["FX N"]
+    metric_data["FX N"] = k_y * metric_data["FX N"]
 
     print(metric_data)
     return metric_data
@@ -105,14 +119,14 @@ def Fz_bin_data(metric_data : pd.DataFrame, Fz_bins : List[List[int]]) -> List[p
         lb = Fz_bin[0]
         ub = Fz_bin[1]
 
-        data_binned = metric_data[(lb <= metric_data["FZ N"]) & (metric_data["FZ N"] < ub)][["SR SAE", "FX N", "FZ N"]]
+        data_binned = metric_data[(lb <= metric_data["FZ N"]) & (metric_data["FZ N"] < ub)][["SL none", "FX N", "FZ N"]]
 
         metric_data_binned.append(data_binned)
     
     return metric_data_binned
 
 def get_pacejka_params(metric_data : pd.DataFrame) -> tuple[float, float, float, float]:
-    params_optimal, _ = curve_fit(pacejka_model, metric_data["SR SAE"], metric_data["FX N"], p0=PACEJKA_PARAMS_GUESS, maxfev=100000)
+    params_optimal, _ = curve_fit(pacejka_model, metric_data["SL none"], metric_data["FX N"], p0=PACEJKA_PARAMS_GUESS, maxfev=100000)
     return params_optimal
 
 def single_fig_color_gradient(metric_data : pd.DataFrame) -> go.Figure:
@@ -120,7 +134,7 @@ def single_fig_color_gradient(metric_data : pd.DataFrame) -> go.Figure:
 
     fig.add_trace(
         go.Scatter(
-            x=metric_data["SR SAE"],
+            x=metric_data["SL none"],
             y=metric_data["FX N"],
             mode="markers", 
             marker=dict(color=metric_data["FZ N"], colorscale="Viridis", showscale=True, colorbar=dict(title="Load Force (N)")),
@@ -134,11 +148,11 @@ def bin_figs(data_binned : List[pd.DataFrame], params_binned : List[tuple[float,
 
     for i in range(FZ_BIN_COUNT):
         data_bin = data_binned[i]
-        graph_domain = np.linspace(min(data_bin["SR SAE"]), max(data_bin["SR SAE"]), 500)
+        graph_domain = np.linspace(min(data_bin["SL none"]), max(data_bin["SL none"]), 500)
 
         # Draw scatterplot of data
         fig.add_trace(
-            go.Scatter( x=data_bin["SR SAE"], 
+            go.Scatter( x=data_bin["SL none"], 
                         y=data_bin["FX N"], 
                         mode="markers", 
                         # marker=dict(
@@ -147,7 +161,7 @@ def bin_figs(data_binned : List[pd.DataFrame], params_binned : List[tuple[float,
                         #     showscale=True, 
                         #     colorbar=dict(title=f"Bin {i + 1}")
                         # ),
-                        name=f'Data for bin {i}'
+                        name=f'Data for bin {i}',
             ), 
             row=i // GRAPH_COLS + 1, 
             col=i % GRAPH_COLS + 1
@@ -172,87 +186,50 @@ def coefficient_figs(params_binned : List[tuple[float, float, float, float, floa
     fig = make_subplots(rows=2, cols=3, subplot_titles=[f"{PACEJKA_PARAMS_NAMES[i]} value over load force bins" for i in range(5)])
 
     for i in range(5):
+        values_for_this_param = params_binned[:,i] # ith column
         Fz_medians = [(ub + lb) / 2 for lb, ub in Fz_bins]
 
-        fig.add_trace(go.Scatter(x=Fz_medians, y=[params_binned[j][i] for j in range(FZ_BIN_COUNT)]), row = i // 3 + 1, col = i % 3 + 1)
+        fig.add_trace(go.Scatter(x=Fz_medians, y=values_for_this_param), row = i // 3 + 1, col = i % 3 + 1)
         fig.update_xaxes(title_text=f"Fz Bin Median", row = i // 3 + 1, col = i % 3 + 1)
         fig.update_yaxes(title_text=f"Value of param {PACEJKA_PARAMS_NAMES[i]}", row = i // 3 + 1, col = i % 3 + 1)
 
-        # Fit param data for the Pacejka model coefficients, we don't have a fit fn for F not sure why
-        if i < 4:
-            coefficients, _ = curve_fit(PACEJKA_PARAM_FIT_FNS[i], Fz_medians, [params_binned[j][i] for j in range(FZ_BIN_COUNT)])
-            Fz_domain = np.linspace(Fz_medians[0], Fz_medians[-1], 50)
-            fig.add_trace(go.Scatter(x=Fz_domain, y=PACEJKA_PARAM_FIT_FNS[i](Fz_domain, *coefficients)), row = i // 3 + 1, col = i % 3 + 1)
+        coefficients, _ = curve_fit(PACEJKA_PARAM_FIT_FNS[i], Fz_medians, values_for_this_param, maxfev=15000000, p0=PACEJKA_PARAM_FIT_GUESS[i])
+        Fz_domain = np.linspace(Fz_medians[0], Fz_medians[-1], 50)
+        fig.add_trace(go.Scatter(x=Fz_domain, y=PACEJKA_PARAM_FIT_FNS[i](Fz_domain, *coefficients)), row = i // 3 + 1, col = i % 3 + 1)
 
-    fig.update_layout(title_text=f"Binned Fx over SR for Fz bins on runs {SELECTED_RUNS}")
+        print(f"Pacejka params for {PACEJKA_PARAMS_NAMES[i]}:", coefficients)
+        print(fit_print(PACEJKA_PARAM_FIT_FNS[i], *coefficients))
+
+    fig.update_layout(title_text=f"Values for Pacejka params over Fz {SELECTED_RUNS}")
 
     return fig
 
-def add_16in_info(data):
-    
-    # calculate slip ratios for 16in tire from 18in data
-    R_18, R_16 = 9, 8
-    data["SR (16in)"] = data["SR SAE"] * (R_18 / R_16)
-        
-    
-    k_y = max(fy.all_data["FY N"]) / max(data["FY N"])
-    data["FX N (16in)"] = k_y * data["FX N"]
-    
-    return data
-
 all_data = get_run_data(SELECTED_RUNS)
-# all_data = all_data[(all_data["FZ N"] > -1000) & (all_data["FZ N"] < -350)]
-all_data = add_16in_info(all_data)
 
-fig = single_fig_color_gradient(all_data)
+special_data = all_data[all_data["SL none"].notnull()]
 
-# print("MIN:", all_data["FZ N"].min())
+# desired_bin = (-np.inf, -900)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(
-    x=all_data[(all_data["FZ N"] > -350) & (all_data["FZ N"] < -0)]["SR (16in)"],
-    y=all_data[(all_data["FZ N"] > -350) & (all_data["FZ N"] < -0)]["FX N (16in)"],
-    # opacity=0.5,
-    mode="markers",
-))
-fig.add_trace(go.Scatter(
-    x=all_data[(all_data["FZ N"] > -800) & (all_data["FZ N"] < -350)]["SR (16in)"],
-    y=all_data[(all_data["FZ N"] > -800) & (all_data["FZ N"] < -350)]["FX N (16in)"],
-    # opacity=0.5,
-    mode="markers",
-))
-fig.add_trace(go.Scatter(
-    x=all_data[(all_data["FZ N"] > -1000) & (all_data["FZ N"] < -800)]["SR (16in)"],
-    y=all_data[(all_data["FZ N"] > -1000) & (all_data["FZ N"] < -800)]["FX N (16in)"],
-    # opacity=0.5,
-    mode="markers", 
-))
-fig.add_trace(go.Scatter(
-    x=all_data[(all_data["FZ N"] > -1500) & (all_data["FZ N"] < -1000)]["SR (16in)"],
-    y=all_data[(all_data["FZ N"] > -1500) & (all_data["FZ N"] < -1000)]["FX N (16in)"],
-    opacity=0.5,
-    mode="markers", 
-))
+# all_data = all_data[(all_data["FZ N"] > desired_bin[0]) & (all_data["FZ N"] < desired_bin[1])]
+# new_fig = single_fig_color_gradient(all_data)
+
+Fz_bins = [(-2000, -900), (-900, -725), (-725, -400), (-400, -250), (-250, 0)] # Empirically found by looking at graph.
+all_data_binned = Fz_bin_data(all_data, Fz_bins)
+
+params_binned = np.array(list(map(get_pacejka_params, all_data_binned)))
+
+bin_fig = bin_figs(all_data_binned, params_binned, Fz_bins)
+coeff_fig = coefficient_figs(params_binned, Fz_bins)
+
+for i in range(FZ_BIN_COUNT):
+    print(f"Bin {i}")
+    print("Params:", params_binned[i])
+
+    r2_val = r2_score(all_data_binned[i]["FX N"], pacejka_model(all_data_binned[i]["SL none"], *params_binned[i]))
+    print("R2:", r2_val)
 
 
-fig.write_html("QVis.html")
-
-# all_data = all_data[all_data["FZ N"]]
-
-# Fz_bins = [(-1300, -1000), (-1000, -750), (-750, -500), (-500, -300), (-300, -150)] # Empirically found by looking at graph.
-# all_data_binned = Fz_bin_data(all_data, Fz_bins)
-
-# params_binned = [get_pacejka_params(data_bin) for data_bin in all_data_binned]
-
-# for i in range(FZ_BIN_COUNT):
-#     print(f"Bin {i}")
-#     print("Params:", params_binned[i])
-
-#     r2_val = r2_score(all_data_binned[i]["FX N"], pacejka_model(all_data_binned[i]["SR SAE"], *params_binned[i]))
-#     print("R2:", r2_val)
-
-
-# new_fig = coefficient_figs(params_binned, Fz_bins)
-# new_fig.write_html("QVis.html")
+bin_fig.write_html("bins_plot.html")
+coeff_fig.write_html("coeff_plot.html")
 
 sys.exit(0)
